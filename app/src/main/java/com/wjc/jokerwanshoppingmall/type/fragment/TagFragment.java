27f95fbe96ea9eb359d0837f5cecade1df13e154
isJoker <m18612008924@163.com>
2016-11-22 22:13:40 +0800

    分类页里面的标签页 GridView

--- app/src/main/java/com/wjc/jokerwanshoppingmall/type/fragment/TagFragment.java
+++ app/src/main/java/com/wjc/jokerwanshoppingmall/type/fragment/TagFragment.java
@@ -1,15 +1,25 @@
 package com.wjc.jokerwanshoppingmall.type.fragment;
 
+import android.util.Log;
 import android.widget.GridView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.wjc.jokerwanshoppingmall.R;
 import com.wjc.jokerwanshoppingmall.base.BaseFragment;
 import com.wjc.jokerwanshoppingmall.type.adapter.TagGridViewAdapter;
 import com.wjc.jokerwanshoppingmall.type.bean.TagBean;
+import com.wjc.jokerwanshoppingmall.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import java.util.List;
 
 import butterknife.Bind;
+import okhttp3.Call;
+import okhttp3.Request;
+
+import static com.wjc.jokerwanshoppingmall.R.id.gv_tag;
 
 /**
  * Created by ${万嘉诚} on 2016/11/22.
@@ -19,7 +29,7 @@ import butterknife.Bind;
 
 public class TagFragment extends BaseFragment {
 
-    @Bind(R.id.gv_tag)
+    @Bind(gv_tag)
     GridView gvTag;
     private TagGridViewAdapter adapter;
     private List<TagBean.ResultBean> result;
@@ -27,6 +37,63 @@ public class TagFragment extends BaseFragment {
     @Override
     public int getViewLayoutId() {
         return R.layout.fragment_tag;
+    }
+
+    @Override
+    public void initData() {
+        getDataFromNet();
+
+    }
+
+    public void getDataFromNet() {
+        OkHttpUtils
+                .get()
+                .url(Constants.TAG_URL)
+                .id(100)
+                .build()
+                .execute(new MyStringCallback());
+    }
+
+    public class MyStringCallback extends StringCallback {
+
+
+        @Override
+        public void onBefore(Request request, int id) {
+        }
+
+        @Override
+        public void onAfter(int id) {
+        }
+
+        @Override
+        public void onError(Call call, Exception e, int id) {
+            Log.e("TAG", "联网失败" + e.getMessage());
+        }
+
+        @Override
+        public void onResponse(String response, int id) {
+
+            switch (id) {
+                case 100:
+//                    Toast.makeText(mContext, "http", Toast.LENGTH_SHORT).show();
+                    if (response != null) {
+                        processData(response);
+                        adapter = new TagGridViewAdapter(mContext, result);
+                        gvTag.setAdapter(adapter);
+                    }
+                    break;
+                case 101:
+                    Toast.makeText(mContext, "https", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+
+    }
+
+    private void processData(String json) {
+        Gson gson = new Gson();
+        TagBean tagBean = gson.fromJson(json, TagBean.class);
+        result = tagBean.getResult();
     }
 
 }
